7005da788c7aabd20fa3a3ca80efad42b0784f68
PPG911 <3136658739@qq.com>
2024-03-20 09:04:29 +0800

    first update

--- src/test/java/testCode.java
+++ src/test/java/testCode.java
@@ -4,9 +4,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
